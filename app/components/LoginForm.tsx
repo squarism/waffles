a73905386c54e8933a75e8ac65b5be174b6816be
Chris Dillon <squarism@gmail.com>
2022-05-25 15:54:36 -0700

    Cargo cult form working

---
+++ app/components/LoginForm.tsx
@@ -0,0 +1,55 @@
+import { Form, Link } from '@remix-run/react'
+import {
+  Stack,
+  TextInput,
+  PasswordInput,
+  Button,
+  Text,
+  Anchor,
+} from '@mantine/core'
+import { At, Lock, Login } from 'tabler-icons-react'
+import type { FC } from 'react'
+import type FormError from '~/types/FormError'
+
+interface Props {
+  loaderData: FormError
+}
+
+const LoginForm: FC<Props> = ({ loaderData }) => {
+  return (
+    <Form method='post' autoComplete='off'>
+      <Stack>
+        <TextInput
+          name='email'
+          label='Email'
+          placeholder='Your Email'
+          icon={<At size={16} />}
+          required
+        />
+        <PasswordInput
+          name='password'
+          label='Password'
+          placeholder='Your Password'
+          icon={<Lock size={16} />}
+          required
+        />
+        {loaderData?.error ? (
+          <Text size='sm' color='red'>
+            {loaderData?.error?.message}
+          </Text>
+        ) : null}
+        <Button type='submit' rightIcon={<Login size={18} />}>
+          Log In
+        </Button>
+        <Text align='center'>
+          Don't have an account?{' '}
+          <Anchor component={Link} to='/signup'>
+            Sign Up.
+          </Anchor>
+        </Text>
+      </Stack>
+    </Form>
+  )
+}
+
+export default LoginForm
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,71 +1,44 @@
-import type {
-  ActionFunction,
-  LoaderFunction,
-} from "@remix-run/node"
 
-import { json } from "@remix-run/node"
+import { json } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+import authenticator from '~/services/auth.server'
+import { sessionStorage } from '~/services/session.server'
+import { Container } from '@mantine/core'
+import LoginForm from '~/components/LoginForm'
+import type { ActionFunction, LoaderFunction } from '@remix-run/node'
+import type FormError from '~/types/FormError'
 
-import { Form, useLoaderData } from "@remix-run/react"
-
-import authenticator from "~/services/auth.server"
-import { sessionStorage } from "~/services/session.server"
-
-// Second, we need to export an action function, here we will use the
-// `authenticator.authenticate method`
-export let action: ActionFunction = async ({ request, context }) => {
-  // we call the method with the name of the strategy we want to use and the
-  // request object, optionally we pass an object with the URLs we want the user
-  // to be redirected to after a success or a failure
-  const resp = await authenticator.authenticate("form", request, {
-    successRedirect: "/",
-    failureRedirect: "/login",
+export const action: ActionFunction = async ({ request, context }) => {
+  const res = await authenticator.authenticate('form', request, {
+    successRedirect: '/',
+    failureRedirect: '/login',
     throwOnError: true,
     context,
   })
-
-  // TODO: remove this
-  console.log(resp)
-  return resp
+  return res
 }
 
 export const loader: LoaderFunction = async ({ request }) => {
   await authenticator.isAuthenticated(request, {
-    successRedirect: "/",
+    successRedirect: '/',
   })
 
   const session = await sessionStorage.getSession(
-    request.headers.get("Cookie")
+    request.headers.get('Cookie')
   )
 
-  const error = session.get("sessionErrorKey")
+  const error = session.get('sessionErrorKey')
   return json<any>({ error })
 }
 
-export default function LoginPage() {
-  const loaderData = useLoaderData()
+const LoginPage = () => {
+  const loaderData = useLoaderData<FormError>()
 
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Login</h1>
-      <p>
-        Based on the Form Strategy From{" "}
-        <a href="https://github.com/sergiodxa/remix-auth" target={"_window"}>
-          Remix-Auth Project
-        </a>
-      </p>
-      <Form method="post">
-        <input type="email" name="email" placeholder="email" required />
-        <input
-          type="password"
-          name="password"
-          placeholder="password"
-          autoComplete="current-password"
-        />
-        <button>Sign In</button>
-      </Form>
-      <div>
-        {loaderData?.error ? <p>ERROR: {loaderData?.error?.message}</p> : null}
-      </div>
-    </div>
+    <Container py="10em" sx={{ maxWidth: "340px" }}>
+      <LoginForm loaderData={loaderData} />
+    </Container>
   )
 }
+
+export default LoginPage
